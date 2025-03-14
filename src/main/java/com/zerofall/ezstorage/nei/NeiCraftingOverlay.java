58bcc3f97fd9f182b5fe1c791473cda23f7bc41b
Pilzinsel64 <pilzinsel64@gmx.de>
2025-03-14 16:06:29 +0100

    overhaul overlayRecipe method

--- src/main/java/com/zerofall/ezstorage/nei/NeiCraftingOverlay.java
+++ src/main/java/com/zerofall/ezstorage/nei/NeiCraftingOverlay.java
@@ -1,6 +1,5 @@
 package com.zerofall.ezstorage.nei;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -30,34 +29,43 @@ public class NeiCraftingOverlay implements IOverlayHandler {
     }
 
     public void overlayRecipe(final GuiContainer gui, final List<PositionedStack> ingredients, final boolean shift) {
-        try {
-            final NBTTagCompound recipe = new NBTTagCompound();
-            if (gui instanceof com.zerofall.ezstorage.gui.GuiCraftingCore) {
-                for (final Iterator<PositionedStack> localIterator1 = ingredients.iterator(); localIterator1
-                    .hasNext();) {
-                    final PositionedStack positionedStack = localIterator1.next();
-                    final int col = (positionedStack.relx - 25) / 18;
-                    final int row = (positionedStack.rely - 6) / 18;
-                    if (positionedStack.items != null && positionedStack.items.length > 0)
-                        for (final Object obj : gui.inventorySlots.inventorySlots) {
-                            final Slot slot = (Slot) obj;
-                            if (slot.inventory instanceof InventoryCrafting && slot.getSlotIndex() == col + row * 3) {
-                                final NBTTagList tags = new NBTTagList();
-                                final List<ItemStack> list = new LinkedList<ItemStack>();
-                                for (int x = 0; x < positionedStack.items.length; x++)
-                                    list.add(positionedStack.items[x]);
-                                for (final ItemStack is : list) {
-                                    final NBTTagCompound tag = new NBTTagCompound();
-                                    is.writeToNBT(tag);
-                                    tags.appendTag(tag);
-                                }
-                                recipe.setTag("#" + slot.getSlotIndex(), tags);
-                                break;
-                            }
-                        }
-                }
-                EZStorage.instance.network.sendToServer(new MsgReqCrafting(recipe));
+        if (!(gui instanceof com.zerofall.ezstorage.gui.GuiCraftingCore)) {
+            return;
+        }
+
+        final NBTTagCompound recipe = new NBTTagCompound();
+
+        for (final PositionedStack positionedStack : ingredients) {
+            if (positionedStack == null || positionedStack.items == null || positionedStack.items.length == 0) {
+                continue;
             }
-        } catch (final Exception localException) {} catch (final Error localError) {}
+
+            final int col = (positionedStack.relx - 25) / 18;
+            final int row = (positionedStack.rely - 6) / 18;
+
+            for (final Slot slot : gui.inventorySlots.inventorySlots) {
+                if (!(slot.inventory instanceof InventoryCrafting) || slot.getSlotIndex() != col + row * 3) {
+                    continue;
+                }
+
+                final NBTTagList tags = new NBTTagList();
+                final List<ItemStack> list = new LinkedList<ItemStack>();
+
+                for (int x = 0; x < positionedStack.items.length; x++) {
+                    list.add(positionedStack.items[x]);
+                }
+
+                for (final ItemStack is : list) {
+                    final NBTTagCompound tag = new NBTTagCompound();
+                    is.writeToNBT(tag);
+                    tags.appendTag(tag);
+                }
+
+                recipe.setTag("#" + slot.getSlotIndex(), tags);
+                break;
+            }
+        }
+
+        EZStorage.instance.network.sendToServer(new MsgReqCrafting(recipe));
     }
 }
